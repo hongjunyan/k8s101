28007310f0123d562f9f6ff7aef135aceffbac96
hongjun <hongjunyan@gmail.com>
2022-11-27 20:59:51 +0800

    use flannel cni

--- init_cluster.sh
+++ init_cluster.sh
@@ -1,7 +1,6 @@
 # Step1: Set control Plane host as apiserver-advertise-address
 master_host = ifconfig eth0 | grep 'inet ' | sed 's/^.*inet //g' | sed 's/ *netmask.*$//g'
-# sudo kubeadm init --apiserver-advertise-address=$master_host  --pod-network-cidr=192.168.0.0/16
-sudo kubeadm init --control-plane-endpoint=$master_host --pod-network-cidr=192.168.0.0/16 --cri-socket=/run/cri-dockerd.sock
+sudo kubeadm init --control-plane-endpoint=$master_host --pod-network-cidr=10.244.0.0/16 --cri-socket=/run/cri-dockerd.sock
 
 # Step2: let user can use kubectl without root privileges
 mkdir -p $HOME/.kube
@@ -9,5 +8,5 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Step3: Installing a Pod network add-on
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/calico.yaml -O
-kubectl apply -f calico.yaml
+curl https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml -O
+kubectl apply -f kube-flannel.yml
